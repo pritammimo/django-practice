a308d8f13a829f777752bc9e161d6eebc8e03b5a
Pritam Saha <57017332+pritammimo@users.noreply.github.com>
2023-04-01 17:29:58 +0000

    django-template add

--- home/views.py
+++ home/views.py
@@ -8,7 +8,9 @@ def home(request):
     {"name":"Pritam","age":26},
     {"name":"Adrija","age":27},
           ]
-  return render(request, "index.html",context={'peoples':people})
+  text="If you did not attempt to sign in to your account, your password may be compromised. Visit https://github.com/settings/security to create a new, strong password for your GitHub account,If you'd like to automatically verify devices in the future, consider enabling two-factor authentication on your account"
+  vegetables=["Pumpkin","tomato","potato"]
+  return render(request, "index.html",context={'peoples':people,"text":text,"vegetables":vegetables})
 
 
 def success_page(request):
