83e8baca0ab1746fd5f1fb1799b61489289e0b89
Pritam Saha <57017332+pritammimo@users.noreply.github.com>
2023-04-02 17:25:12 +0000

    crud operation with shell

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True,blank=True)
+    email = models.EmailField()
+    address = models.TextField()
+    # image=models.ImageField()
+class Car(models.Model):
+  car_name=models.CharField(max_length=100)
+  speed=models.IntegerField(default=50)
