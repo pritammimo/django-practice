be5674bde16efd058748ba91e1c8bafabf1c373b
pritammimo <5628760-pritammimo@users.noreply.replit.com>
2023-04-08 17:31:00 +0000

    new upload

--- vege/views.py
+++ vege/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def receipes(request):
+  if request.method == "POST":
+    data=request.POST
+    receipe_name=data.get("receipe_name")
+    receipe_description=data.get("receipe_description")
+    receipe_image=request.FILES.get("receipe_image")
+    Receipe.objects.create(
+      receipe_name=receipe_name,
+      receipe_image=receipe_image,
+      receipe_description=receipe_description
+    )
+    print(receipe_name)
+    print(receipe_image)
+    print(receipe_description)
+  return render(request,'receipes.html')
